c4ac7c6c2cd6ef0fb249bc98f716b8aeffce9e38
Shubham-Bhayaje <166942712+Shubham-Bhayaje@users.noreply.github.com>
2024-06-20 16:21:28 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -26,6 +26,7 @@ export default function Home() {
       throw error;
     }
   }
+  
   useEffect(() => {
     fetchWeather();
   }, []);
@@ -43,94 +44,72 @@ export default function Home() {
 
   return (
     <div
-      className="h-screen w-screen flex  items-center justify-center"
+      className="min-h-screen flex items-center justify-center"
       style={{
         backgroundImage: `url("./pexels-donaldtong94-133953.jpg")`,
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
     >
-      <div className="bg-slate-900 sm:h-96 sm:w-96 rounded-lg flex flex-col items-center justify-center lg:h-4/6 lg:w-1/3">
-        <div className="flex items-center justify-center w-96 mt-6">
+      <div className="bg-slate-900 rounded-lg flex flex-col items-center justify-center p-4 w-full max-w-md sm:max-w-lg lg:max-w-xl">
+        <div className="flex items-center justify-center w-full mt-6">
           <input
             type="text"
-            className="h-10  rounded-lg text-black pl-4 font-serif w-72 "
+            className="h-10 rounded-lg text-black pl-4 font-serif w-3/4 sm:w-2/3 lg:w-1/2"
             value={location}
             onChange={(event) => {
               setLocation(event.target.value);
             }}
           ></input>
-          <button onClick={fetchWeather} className="rounded-full">
+          <button onClick={fetchWeather} className="rounded-full ml-2">
             <img
-              onClick={fetchWeather}
               src="./search-svgrepo-com.svg"
-              className="w-10 h-10  p-2 ml-2 rounded-lg "
+              className="w-10 h-10 p-2 rounded-lg"
               style={{ backgroundColor: "#47b59b" }}
             ></img>
           </button>
         </div>
         {weather && current && (
-          <div className="w-80 h-10 m-10 mb-0 text-center mt-6">
-            <h1 className=" text-4xl text-white font-serif" value="">
-              {weather.name}
-            </h1>
-          </div>
+          <>
+            <div className="text-center mt-6">
+              <h1 className="text-4xl text-white font-serif">{weather.name}</h1>
+              <p className="text-slate-500">{formatDate(weather.localtime)}</p>
+            </div>
+            <div className="flex items-center justify-center mt-4">
+              <img src={current.condition.icon} className="w-8 h-8"></img>
+              <p className="text-white m-1">{current.condition.text}</p>
+            </div>
+            <div className="text-center mt-4">
+              <p className="text-white text-4xl font-bold">
+                {current.temp_c}
+                <span className="text-[30px]">&deg;</span>
+              </p>
+            </div>
+            <div className="flex items-center justify-center gap-4 mt-6 mb-8">
+              <div className="w-28 h-28 bg-white rounded-lg flex flex-col items-center justify-center p-2">
+                <img src="./wind.png" className="w-8 h-8"></img>
+                <h2 className="font-medium text-customGreen">
+                  {current.wind_kph} Km/h
+                </h2>
+                <h5 className="font-serif">Wind</h5>
+              </div>
+              <div className="w-28 h-28 bg-white rounded-lg flex flex-col items-center justify-center p-2">
+                <img src="./drop.png" className="w-8 h-8"></img>
+                <h2 className="font-medium text-customGreen">
+                  {current.humidity}%
+                </h2>
+                <h5 className="font-serif">Humidity</h5>
+              </div>
+              <div className="w-28 h-28 bg-white rounded-lg flex flex-col items-center justify-center p-2">
+                <img src="./eye.png" className="w-8 h-8"></img>
+                <h2 className="font-medium text-customGreen">
+                  {current.vis_km} Km
+                </h2>
+                <h5 className="font-serif">Visibility</h5>
+              </div>
+            </div>
+          </>
         )}
-        <div className="w-full h-6 m-2 flex items-center justify-center">
-          {weather && current && (
-            <p className="text-slate-500">{formatDate(weather.localtime)}</p>
-          )}
-        </div>
-        <div className="w-full h-6  m-2 flex items-center justify-center">
-          {weather && current && (
-            <img src={current.condition.icon} className="w-8 h-8"></img>
-          )}
-          {weather && current && (
-            <p className="text-white m-1">{current.condition.text}</p>
-          )}
-        </div>
-        <div className="w-full h-16  flex items-center justify-center">
-          {" "}
-          {weather && current && (
-            <p className="text-white text-4xl font-bold flex items-center justify-cente">
-              {current.temp_c}
-              <span className="text-[30px]">&deg;</span>
-            </p>
-          )}
-        </div>
-
-        <div className="w-[100%] mt-6 h-28 flex items-center justify-center gap-4 mb-8">
-          <div className="w-24 h-24 bg-white rounded-lg flex flex-col items-center justify-center">
-            <img src="./wind.png" className="w-8 h-8 m-2"></img>
-            {weather && current && (
-              <h2 className="font-medium text-customGreen">
-                {" "}
-                {current.wind_kph} Km/h
-              </h2>
-            )}
-            <h5 className="font-serif">Wind</h5>
-          </div>
-          <div className="w-24 h-24 bg-white rounded-lg flex flex-col items-center justify-center">
-            <img src="./drop.png" className="w-8 h-8 m-2"></img>
-            {weather && current && (
-              <h2 className="font-medium text-customGreen">
-                {" "}
-                {current.humidity}%
-              </h2>
-            )}
-            <h5 className="font-serif">Humidity</h5>
-          </div>
-          <div className="w-24 h-24 bg-white rounded-lg flex flex-col items-center justify-center">
-            <img src="./eye.png" className="w-8 h-8 m-2"></img>
-            {weather && current && (
-              <h2 className="font-medium text-customGreen">
-                {" "}
-                {current.vis_km} Km
-              </h2>
-            )}
-            <h5 className="font-serif">Visiblity</h5>
-          </div>
-        </div>
       </div>
     </div>
   );
